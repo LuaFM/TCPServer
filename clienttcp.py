b0fb96c69dfb4e437cbd4d53ee7835ebeac5be47
Luana FM <37968766+LuanaFM@users.noreply.github.com>
2018-04-07 23:11:13 -0300

    Update clienttcp.py

--- clienttcp.py
+++ clienttcp.py
@@ -4,14 +4,14 @@ import socket, os
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   # AF_INET = IPV4 e SOCK_STREAM = TCP
 
 # Escolha IP e Porta onde irá levantar o Cliente.
-ip = str(input('Informe o IP onde o cliente será criado: '))
+ip = str(input('Informe o IP onde o cliente fará a requisição: '))
 porta = int(input('Digite a porta a ser usada pela conexão: '))
 
 client.connect((ip, porta))
 
 try:
-    while True:                                 # Para criar uma conexão com chat
-        msg = input('Digite algo: ') + "\n"
+    while True:                                 # Para criar uma conexão com chat ou mandar a requisição para o Host
+        msg = input('Digite: ') + "\n"
         client.send(msg.encode())               # No python 3 é necessário utilizar o encode e o decode
         res = client.recv(1048)
         print(res.decode() + '\n')
